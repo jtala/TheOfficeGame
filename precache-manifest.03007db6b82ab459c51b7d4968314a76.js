cc639b8ed23779d08de3c1e9d2c4e2f99347af41
Jonathan Talavera <jtalavera@g.ucla.edu>
2019-02-05 09:49:42 -0800

    Updates

---
+++ precache-manifest.03007db6b82ab459c51b7d4968314a76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3ee0782a3f85d134f2ce8b4364263d7",
+    "url": "/TheOfficeGame/static/media/ryan.d3ee0782.jpg"
+  },
+  {
+    "revision": "9654ea4178baf1827b2d",
+    "url": "/TheOfficeGame/static/js/runtime~main.9654ea41.js"
+  },
+  {
+    "revision": "41b6e063857d7864d674",
+    "url": "/TheOfficeGame/static/js/main.41b6e063.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/TheOfficeGame/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "41b6e063857d7864d674",
+    "url": "/TheOfficeGame/static/css/main.9b8804fc.chunk.css"
+  },
+  {
+    "revision": "a33d718aa4dd0cf217e4ee65481c4c2f",
+    "url": "/TheOfficeGame/index.html"
+  }
+];
