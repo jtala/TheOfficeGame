c9c8aeabbcce0477e948a8e157cb6898afc03004
Jonathan Talavera <jonathantalavera@Jonathans-MacBook-Pro.local>
2019-02-06 15:06:43 -0800

    Updates

---
+++ precache-manifest.73c3122feab0b713ec6ccf73e104bfeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f3081ab34bd3c4c3b37",
+    "url": "/TheOfficeGame/static/css/main.ebe3e57f.chunk.css"
+  },
+  {
+    "revision": "1f3081ab34bd3c4c3b37",
+    "url": "/TheOfficeGame/static/js/main.1f3081ab.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/TheOfficeGame/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "9654ea4178baf1827b2d",
+    "url": "/TheOfficeGame/static/js/runtime~main.9654ea41.js"
+  },
+  {
+    "revision": "d3ee0782a3f85d134f2ce8b4364263d7",
+    "url": "/TheOfficeGame/static/media/ryan.d3ee0782.jpg"
+  },
+  {
+    "revision": "546e6cc4b4bd681cf50d424988e25b3a",
+    "url": "/TheOfficeGame/index.html"
+  }
+];
